c65254945588601590900ac03462a75fcafbe890
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-03-13 16:45:12 -0300

    moving our hero in map limiting our hero's movement to the map boundaries

--- fogefoge.rb
+++ fogefoge.rb
@@ -11,17 +11,43 @@ end
 def encontra_jogador(mapa)
   caracter_do_heroi = 'H'
 
-  for linha in 0..(mapa.size - 1)
-    linha_atual = mapa[linha]
+  mapa.each_with_index do |linha_atual, linha|
+    coluna_do_heroi = linha_atual.index caracter_do_heroi
 
-    for coluna in 0..(linha_atual.size - 1)
-      heroi_esta_aqui = linha_atual[coluna] == caracter_do_heroi
-      if heroi_esta_aqui
-        # achei!
-      end
+    if coluna_do_heroi
+      return [linha, coluna_do_heroi]
     end
   end
   # não achei
+end
+
+def calcula_nova_posicao(heroi, direcao)
+  heroi = heroi.dup
+
+  case direcao
+  when 'W'
+    heroi[0] -= 1
+  when 'S'
+    heroi[0] += 1
+  when 'A'
+    heroi[1] -= 1
+  when 'D'
+    heroi[1] += 1
+  end
+
+  heroi
+end
+
+def posicao_valida?(mapa, posicao)
+  linhas = mapa.size
+  colunas = mapa[0].size
+
+  estourou_linha  = posicao[0].negative? || posicao[0] >= linhas
+  estourou_coluna = posicao[1].negative? || posicao[1] >= colunas
+
+  return false if mapa[posicao[0]][posicao[1]] == 'X' || estourou_linha || estourou_coluna
+
+  true
 end
 
 def joga(nome)
@@ -30,6 +56,14 @@ def joga(nome)
   while true
     desenha mapa
     direcao = pede_movimento
+    heroi = encontra_jogador mapa
+
+    nova_posicao = calcula_nova_posicao heroi, direcao
+
+    next if !posicao_valida? mapa, nova_posicao
+
+    mapa[heroi[0]][heroi[1]] = ' '
+    mapa[nova_posicao[0]][nova_posicao[1]] = 'H'
   end
 
 end

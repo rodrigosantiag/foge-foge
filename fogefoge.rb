5b67990b05dba75f4a2b0b0b3c4d2aad20907f3b
Rodrigo Santiago <rodrigosantiag@gmail.com>
2021-03-15 19:51:23 -0300

    Moving ghosts (only moving right for now) Validating position for ghosts

--- fogefoge.rb
+++ fogefoge.rb
@@ -43,13 +43,39 @@ def posicao_valida?(mapa, posicao)
   estourou_linha  = posicao[0].negative? || posicao[0] >= linhas
   estourou_coluna = posicao[1].negative? || posicao[1] >= colunas
 
-  return false if mapa[posicao[0]][posicao[1]] == 'X' || estourou_linha || estourou_coluna
+  return false if estourou_linha || estourou_coluna
+
+  valor_atual = mapa[posicao[0]][posicao[1]]
+
+  return false if valor_atual == 'X' || valor_atual == 'F'
 
   true
 end
 
+def move_fantasma(mapa, linha, coluna)
+  posicao = [linha, coluna + 1]
+  if posicao_valida? mapa, posicao
+    mapa[linha][coluna] = ' '
+    mapa[posicao[0]][posicao[1]] = 'F'
+  end
+end
+
+def move_fantasmas(mapa)
+  caractere_do_fantasma = 'F'
+
+  mapa.each_with_index do |linha_atual, linha|
+    linha_atual.chars.each_with_index do |caracter_atual, coluna|
+      eh_fantasma = caracter_atual == caractere_do_fantasma
+
+      if eh_fantasma
+        move_fantasma mapa, linha, coluna
+      end
+    end
+  end
+end
+
 def joga(nome)
-  mapa = le_mapa 1
+  mapa = le_mapa 2
 
   while true
     desenha mapa
@@ -62,6 +88,8 @@ def joga(nome)
 
     mapa[heroi[0]][heroi[1]] = ' '
     mapa[nova_posicao[0]][nova_posicao[1]] = 'H'
+
+    move_fantasmas mapa
   end
 
 end
